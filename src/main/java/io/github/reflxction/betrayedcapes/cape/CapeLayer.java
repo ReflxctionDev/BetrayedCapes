ee973a853ab7f0254f6acf5d0a7debe4722173b5
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2020-02-13 21:04:01 +0200

    Refactor fields to use un-obfuscated names

--- src/main/java/io/github/reflxction/betrayedcapes/cape/CapeLayer.java
+++ src/main/java/io/github/reflxction/betrayedcapes/cape/CapeLayer.java
@@ -20,7 +20,6 @@ import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EnumPlayerModelParts;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
@@ -28,7 +27,7 @@ import net.minecraft.util.ResourceLocation;
 /**
  * The cape layer which handles cape rendering
  */
-public class CapeLayer implements LayerRenderer {
+public class CapeLayer implements LayerRenderer<AbstractClientPlayer> {
 
     // Player to render the cape for
     private final RenderPlayer playerRenderer;
@@ -46,8 +45,7 @@ public class CapeLayer implements LayerRenderer {
     }
 
     @Override
-    public void doRenderLayer(EntityLivingBase entity, float p_177141_2_, float p_177141_3_, float partialTicks, float p_177141_5_, float p_177141_6_, float p_177141_7_, float scale) {
-        AbstractClientPlayer player = ((AbstractClientPlayer) entity);
+    public void doRenderLayer(AbstractClientPlayer player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
         ResourceLocation rl = BetrayedCapes.getCache().contains(player.getUniqueID()) ? downloader.getCapeTexture() : null;
         if (player.hasSkin() && !player.isInvisible() && player.isWearing(EnumPlayerModelParts.CAPE)) {
             float f9 = 0.14F;
@@ -65,8 +63,8 @@ public class CapeLayer implements LayerRenderer {
             double d1 = player.prevChasingPosY + (player.chasingPosY - player.prevChasingPosY) * (double) partialTicks - (player.prevPosY + (player.posY - player.prevPosY) * (double) partialTicks);
             double d2 = player.prevChasingPosZ + (player.chasingPosZ - player.prevChasingPosZ) * (double) partialTicks - (player.prevPosZ + (player.posZ - player.prevPosZ) * (double) partialTicks);
             float f = player.prevRenderYawOffset + (player.renderYawOffset - player.prevRenderYawOffset) * partialTicks;
-            double d3 = (double) MathHelper.sin(f * 0.017453292F);
-            double d4 = (double) (-MathHelper.cos(f * 0.017453292F));
+            double d3 = MathHelper.sin(f * 0.017453292F);
+            double d4 = -MathHelper.cos(f * 0.017453292F);
             float f1 = (float) d1 * 10.0F;
             f1 = MathHelper.clamp_float(f1, 3.0F, 32.0F);
             float f2 = (float) (d0 * d3 + d2 * d4) * 100.0F;
